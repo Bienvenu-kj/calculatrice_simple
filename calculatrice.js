d4241f331a2aa94520e249445ee4f2f60dcdf2ac
Bienvenu Kajirifo Luc <bienvenukajirifoluc@gmail.com>
2024-12-03 17:10:49 +0200

    Update calculatrice.js

--- calculatrice.js
+++ calculatrice.js
@@ -1,11 +1,20 @@
 // ceci est une calculatrice faite en typeScript, conjoitement avec tailwindcss
 // selection des éléments d'affichage
+let information_orientation= document.querySelector('#information_orientation');
 let displayCharacter = document.querySelector('#displayCharacter');
 let displayResult = document.querySelector('#displayResult');
 let buttons = document.querySelectorAll('.bouton');
 // let deleteCharButton = document.querySelector('#deleteChar');
 // let deleteAllButton = document.querySelector('#deleteAll');
 // let resultbutton = document.querySelector('#result');
+
+window.addEventListener("orientationchange", () => {
+    if (window.orientation === 90 || window.orientation === -90) {
+     information_orientation.style.display='block';
+body.style.backgroundColor = "black";
+    }
+});
+
 function ajouterCaractère(character) {
     return displayCharacter.innerHTML += character;
 }
